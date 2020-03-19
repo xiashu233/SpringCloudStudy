d34c6753921636bcb310c0bcdd866488f37ae39a
李治强 <2453893123@qq.com>
2020-03-19 13:01:31 +0800

    🔨 重构项目 将Service和 bean提取出来

--- cloud2020/cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
+++ cloud2020/cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor // 空参构造方法
 @NoArgsConstructor  // 全参构造方法
-public class CommonResult<T> {
+public class CommonResult<T> implements Serializable {
     // 404 not_found data
     private Integer code;
     private String message;
