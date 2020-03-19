0b0b54e2612c5ea5d89d4bf4cd314789d91412b1
李治强 <2453893123@qq.com>
2020-03-19 22:01:19 +0800

    🏚 服务间负载均衡

---
+++ cloud2020/cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/dao/PaymentDao.java
@@ -0,0 +1,12 @@
+package com.atguigu.springcloud.dao;
+
+import com.atguigu.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface PaymentDao {
+    public int Insert(Payment payment);
+
+    public Payment SelectPaymentById(@Param("id") Long id);
+}
