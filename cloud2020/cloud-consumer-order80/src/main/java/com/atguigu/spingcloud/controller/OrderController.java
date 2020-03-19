150ed74446b61111f69643d5eebe2fb6923dde36
李治强 <2453893123@qq.com>
2020-03-19 13:02:40 +0800

    ✒ 创建注册中心

---
+++ cloud2020/cloud-consumer-order80/src/main/java/com/atguigu/spingcloud/controller/OrderController.java
@@ -0,0 +1,36 @@
+package com.atguigu.spingcloud.controller;
+
+
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderController {
+
+    public static final String PAYMENT_URL = "http://localhost:8001";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @ResponseBody
+    @GetMapping("/consumer/payment/insert")
+    public CommonResult<Payment> insert(Payment payment){
+        return restTemplate.postForObject(PAYMENT_URL + "/payment/insert",payment, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/"+id,CommonResult.class);
+    }
+
+}
--- cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
@@ -3,7 +3,6 @@ package com.atguigu.springcloud.service.impl;
 import com.atguigu.springcloud.dao.PaymentDao;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
